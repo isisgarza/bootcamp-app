05b020eda9969733f1df5e5d17e5bef4c729892c
EC2 Default User <ec2-user@ip-172-31-78-161.ec2.internal>
2021-06-04 02:28:52 +0000

    Fix of app.py file

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,7 @@ CORS(app)
 # sent to the service root will receive a healthy response.
 @app.route("/")
 def healthCheckResponse():
-    return jsonify({"message": "Version 1. Nothing here, used health many checks. Try /mysfits instead."})
+    return jsonify({"message": "Version 1. Nothing here, used health check. Try /mysfits instead."})
 
 # The main API resource that the next version of the Mythical Mysfits website
 # will utilize. It returns the data for all of the Mysfits to be displayed on
